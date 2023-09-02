d7c4b9eed0e7eaf6848498e64b3de558579c8bd6
Cogan <coganizz@gmail.com>
2023-09-02 22:02:20 +0300

    First

---
+++ src/game.js
@@ -0,0 +1,21 @@
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export default Game;
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+import Character from './domain.js';
+export { GameSavingData, readGameSaving, writeGameSaving };
+
+
